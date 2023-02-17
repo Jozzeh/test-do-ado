ba64b1b1081428b4afde8fa264884ae416debf44
Jos De Berdt <jos.deberdt@sweetmustard.be>
2023-02-17 13:43:24 +0100

    ts bad boy

--- config/database.ts
+++ config/database.ts
@@ -8,7 +8,6 @@
 import Env from '@ioc:Adonis/Core/Env'
 import Application from '@ioc:Adonis/Core/Application'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
-import fs from 'fs'
 
 const databaseConfig: DatabaseConfig = {
   /*
